832ddb09278d62f50ce2e62fc40954cc461ef176
Poscat <poscat@mail.poscat.moe>
2020-04-09 18:24:11 +0800

    Export InputMessageContent

--- src/Web/Telegram/Types/Inline.hs
+++ src/Web/Telegram/Types/Inline.hs
@@ -2,7 +2,9 @@ module Web.Telegram.Types.Inline
   ( InlineQueryResult (..),
     InlineQuery (..),
     ChosenInlineResult (..),
+    InputMessageContent (..),
   )
 where
 
 import Web.Telegram.Types.Internal.InlineQuery
+import Web.Telegram.Types.Internal.InputMedia
