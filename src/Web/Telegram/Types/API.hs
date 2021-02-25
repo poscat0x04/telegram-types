c09ccc81cff10399538894cf2d1273022c797e18
Poscat <poscat@mail.poscat.moe>
2021-02-25 13:52:15 +0800

    Add result type

--- src/Web/Telegram/Types/API.hs
+++ src/Web/Telegram/Types/API.hs
@@ -1,8 +1,10 @@
 module Web.Telegram.Types.API
-  ( module X,
+  ( Result (..),
+    module X,
   )
 where
 
+import Common hiding (Result (..))
 import Web.Telegram.Types.API.ChatActions as X
 import Web.Telegram.Types.API.ChatId as X
 import Web.Telegram.Types.API.Commands as X
@@ -10,3 +12,30 @@ import Web.Telegram.Types.API.Messages as X
 import Web.Telegram.Types.API.Queries as X
 import Web.Telegram.Types.API.Stickers as X
 import Web.Telegram.Types.API.Updates as X
+import Web.Telegram.Types.Internal.ResponseParameters
+
+-- | The result of an API request
+data Result a
+  = Success a
+  | Failure
+      { description :: Text,
+        errorCode :: Int,
+        parameters :: Maybe ResponseParameters
+      }
+  deriving stock (Show, Eq)
+
+makePrismLabels ''Result
+mkLabel ''Result
+
+instance FromJSON a => FromJSON (Result a) where
+  parseJSON = withObject "telegram bot api request result" $ \o -> do
+    ok <- o .: "ok"
+    if ok
+      then do
+        result <- o .: "result"
+        Success <$> parseJSON result
+      else do
+        description <- o .: "description"
+        errorCode <- o .: "error_code"
+        parameters <- o .: "parameters"
+        pure Failure {..}
